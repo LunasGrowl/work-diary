dc779b9e68a955637ca41311edf1db7072b84434
LunasGrowl <abychawski@gmail.com>
2024-08-16 11:49:24 +0100

    changed location of comonents

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import NavBar from './Nav.jsx'
-import DiaryEntry from './DiaryEntry.jsx'
-import DiaryInput from './DiaryInput.jsx'
+import NavBar from './components/Nav.jsx'
+import DiaryEntry from './components/DiaryEntry.jsx'
+import DiaryInput from './components/DiaryInput.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
