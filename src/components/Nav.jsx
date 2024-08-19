b64b4c49af1d6a62da162cb06c38006501bcc2eb
LunasGrowl <abychawski@gmail.com>
2024-08-19 10:37:51 +0100

    nav bar update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import "./Nav.css";
 
 const NavBar = () => {
   return (
-    <div id= "nav-bar"><p>Aleks Tata Diary</p></div>
+    <div id= "nav--bar" className='bg-zinc-800 sticky top-0' >
+      <p>Aleks Tata Diary</p>
+    </div>
   )
 }
 
