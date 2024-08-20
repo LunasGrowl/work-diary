f2a08d3e0b6e1551be0c4aae059ca2b111ce9c23
LunasGrowl <abychawski@gmail.com>
2024-08-20 15:11:16 +0100

    back end init

---
+++ backend/target/classes/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS entry (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  entry_day varchar(255),
+  entry_date varchar(255),
+  entry_content varchar(255)
+  );
+
+ -- Insert into entry Values (1,'dsa','dsadsad','321321');
